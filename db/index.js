385e065edec8cb9c6f07c90b2e073709b0486e40
Armin <arminch1997@gmail.com>
2021-06-22 13:04:32 -0700

    code

--- db/index.js
+++ db/index.js
@@ -1,7 +1,3 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_NAME, process.env.LOCALDB_USER, process.env.LOCALDB_PASS,{
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-})
+module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_URL)
